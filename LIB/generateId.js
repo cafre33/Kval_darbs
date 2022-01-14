74cd382262bf8c11e47bec1d6374d7636f6de18f
cafre33 <97725743+cafre33@users.noreply.github.com>
2022-01-14 09:51:26 +0200

    Create generateId.js

---
+++ LIB/generateId.js
@@ -0,0 +1,9 @@
+//Uzģenerē identifikatoru abpusēji ieinteresēta lietotāja dokumentam
+const generateId= (id1, id2) => {
+    if(id1>id2){
+        return id1+id2;
+    }else {
+        return id2+id1;
+    }
+}
+export default generateId;
